2d7a8710ad981515b1ec923ca26aa9df1860bd3f
Julian Frimmel <julian.frimmel@gmail.com>
2019-07-31 21:21:01 +0200

    Added core XML strucutre.

--- src/valgrind_xml/mod.rs
+++ src/valgrind_xml/mod.rs
@@ -1,2 +1,38 @@
 //! A module containing the structure of the valgrind XML output.
+//!
+//! Note, that not all fields are implemented.
+#[cfg(test)]
+mod tests;
 
+use serde::Deserialize;
+
+/// The output of a valgrind run.
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
+#[serde(rename = "valgrindoutput")]
+pub struct Output {
+    #[serde(rename = "error")]
+    errors: Vec<Error>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
+struct Error {
+    kind: Kind,
+    #[serde(rename = "xwhat")]
+    resources: Resources,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
+enum Kind {
+    #[serde(rename = "Leak_DefinitelyLost")]
+    DefinitelyLost,
+    #[serde(rename = "Leak_StillReachable")]
+    StillReachable,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
+struct Resources {
+    #[serde(rename = "leakedbytes")]
+    bytes: usize,
+    #[serde(rename = "leakedblocks")]
+    blocks: usize,
+}
