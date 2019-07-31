bbe47d4872dfae66830da33015cbc30fe9858419
Julian Frimmel <julian.frimmel@gmail.com>
2019-07-31 16:37:02 +0200

    Added function to collect the crate metadata.
    This uses the `cargo metadata` command.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,33 @@
 //! The core library of the `cargo-valgrind` command.
 mod metadata;
+
+use std::{path::Path, io, process::Command};
+
+/// Run the `cargo metadata` command and collect its output.
+///
+/// The `path` has to point to the `Cargo.toml` of which the metadata should be
+/// collected. Metadata of the dependencies is omitted on purpose. The output is
+/// then converted into a `String`.
+///
+/// # Errors
+/// This function can fail either because the `cargo metadata` command could not
+/// be spawned, the command failed (i.e. it was executed but returned a non-zero
+/// exit code) or the string printed to stdout was not valid UTF-8.
+fn cargo_metadata<P: AsRef<Path>>(path: P) -> Result<String, io::Error> {
+    let output = Command::new("cargo")
+        .arg("metadata")
+        .arg("--format-version=1")
+        .arg("--no-deps")
+        .arg("--offline")
+        .arg("--manifest-path")
+        .arg(path.as_ref())
+        .output()?;
+
+    if output.status.success() {
+        String::from_utf8(output.stdout)
+            .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "Non-UTF-8 string"))
+    } else {
+        dbg!(String::from_utf8(output.stderr).unwrap());
+        Err(io::Error::new(io::ErrorKind::Other, "cargo command failed"))
+    }
+}
