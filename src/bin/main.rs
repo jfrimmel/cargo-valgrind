6c4fb4f53ff7aed557b406d9d1bc6b1ccb1666c1
Julian Frimmel <julian.frimmel@gmail.com>
2019-07-31 17:55:57 +0200

    Updated main to look for debug builds.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,12 @@
 use cargo_valgrind::{binaries, Build};
 
 fn main() {
-    for binary in binaries("Cargo.toml", Build::Debug).unwrap() {
-        println!("{}", binary.to_str().unwrap());
+    match binaries("Cargo.toml", Build::Debug) {
+        Ok(binaries) => {
+            for binary in binaries {
+                println!("{}", binary.to_str().unwrap());
+            }
+        }
+        Err(e) => eprintln!("Error: {}", e),
     }
 }
