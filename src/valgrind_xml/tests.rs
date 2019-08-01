95753474e2991e3ae4c8d1521d9006797f18b83b
Julian Frimmel <julian.frimmel@gmail.com>
2019-08-01 09:11:49 +0200

    Added tests for deserializing prefix-hex numbers.

--- src/valgrind_xml/tests.rs
+++ src/valgrind_xml/tests.rs
@@ -1,4 +1,4 @@
-use super::{Frame, Kind, Output, Resources};
+use super::{Error, Frame, Kind, Output, Resources};
 
 #[test]
 fn sample_output() {
@@ -48,4 +48,155 @@ fn sample_output() {
             blocks: 1,
         }
     );
+}
+
+#[test]
+fn unique_ids_have_to_be_in_hex_with_prefix() {
+    let result: Error = serde_xml_rs::from_str(
+        r"<error>\
+           <unique>0xDEAD1234</unique>\
+           <tid>1</tid>\
+           <kind>Leak_DefinitelyLost</kind>\
+           <xwhat>\
+           <text>...</text>\
+             <leakedbytes>15</leakedbytes>\
+             <leakedblocks>1</leakedblocks>\
+           </xwhat>\
+           <stack>\
+             <frame>\
+               <ip>0x483AD7B</ip>\
+             </frame>\
+           </stack>\
+         </error>",
+    )
+    .unwrap();
+    assert_eq!(result.unique, 0xDEAD_1234);
+}
+
+#[test]
+fn missing_hex_prefix_is_an_error() {
+    let result: Result<Error, _> = serde_xml_rs::from_str(
+        r"<error>\
+           <unique>0DEADBEEF</unique>\
+           <tid>1</tid>\
+           <kind>Leak_DefinitelyLost</kind>\
+           <xwhat>\
+           <text>...</text>\
+             <leakedbytes>15</leakedbytes>\
+             <leakedblocks>1</leakedblocks>\
+           </xwhat>\
+           <stack>\
+             <frame>\
+               <ip>0x483AD7B</ip>\
+             </frame>\
+           </stack>\
+         </error>",
+    );
+    assert!(result.is_err());
+
+    let result: Result<Error, _> = serde_xml_rs::from_str(
+        r"<error>\
+           <unique>xDEADBEEF</unique>\
+           <tid>1</tid>\
+           <kind>Leak_DefinitelyLost</kind>\
+           <xwhat>\
+           <text>...</text>\
+             <leakedbytes>15</leakedbytes>\
+             <leakedblocks>1</leakedblocks>\
+           </xwhat>\
+           <stack>\
+             <frame>\
+               <ip>0x483AD7B</ip>\
+             </frame>\
+           </stack>\
+         </error>",
+    );
+    assert!(result.is_err());
+
+    let result: Result<Error, _> = serde_xml_rs::from_str(
+        r"<error>\
+           <unique>DEADBEEF</unique>\
+           <tid>1</tid>\
+           <kind>Leak_DefinitelyLost</kind>\
+           <xwhat>\
+           <text>...</text>\
+             <leakedbytes>15</leakedbytes>\
+             <leakedblocks>1</leakedblocks>\
+           </xwhat>\
+           <stack>\
+             <frame>\
+               <ip>0x483AD7B</ip>\
+             </frame>\
+           </stack>\
+         </error>",
+    );
+    assert!(result.is_err());
+}
+
+#[test]
+fn invalid_hex_digits_are_an_error() {
+    let result: Result<Error, _> = serde_xml_rs::from_str(
+        r"<error>\
+           <unique>0xhello</unique>\
+           <tid>1</tid>\
+           <kind>Leak_DefinitelyLost</kind>\
+           <xwhat>\
+           <text>...</text>\
+             <leakedbytes>15</leakedbytes>\
+             <leakedblocks>1</leakedblocks>\
+           </xwhat>\
+           <stack>\
+             <frame>\
+               <ip>0x483AD7B</ip>\
+             </frame>\
+           </stack>\
+         </error>",
+    );
+    assert!(result.is_err());
+}
+
+#[test]
+fn hex_and_prefix_case_is_ignored() {
+    let result: Error = serde_xml_rs::from_str(
+        r"<error>\
+           <unique>0XdEaDbEeF</unique>\
+           <tid>1</tid>\
+           <kind>Leak_DefinitelyLost</kind>\
+           <xwhat>\
+           <text>...</text>\
+             <leakedbytes>15</leakedbytes>\
+             <leakedblocks>1</leakedblocks>\
+           </xwhat>\
+           <stack>\
+             <frame>\
+               <ip>0x483AD7B</ip>\
+             </frame>\
+           </stack>\
+         </error>",
+    )
+    .unwrap();
+    assert_eq!(result.unique, 0xDEAD_BEEF);
+}
+
+#[test]
+fn unique_id_is_64bit() {
+    let result: Error = serde_xml_rs::from_str(
+        r"<error>\
+           <unique>0x123456789ABCDEF0</unique>\
+           <tid>1</tid>\
+           <kind>Leak_DefinitelyLost</kind>\
+           <xwhat>\
+           <text>...</text>\
+             <leakedbytes>15</leakedbytes>\
+             <leakedblocks>1</leakedblocks>\
+           </xwhat>\
+           <stack>\
+             <frame>\
+               <ip>0x483AD7B</ip>\
+             </frame>\
+           </stack>\
+         </error>",
+    )
+    .unwrap();
+    assert_eq!(result.unique, 0x1234_5678_9ABC_DEF0);
 }
