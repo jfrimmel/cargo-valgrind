a5e225e00b2e024c04da31d6d35d0573176072f9
Julian Frimmel <julian.frimmel@gmail.com>
2020-03-26 20:20:58 +0100

    Move driver functionality to submodule

--- src/main.rs
+++ src/main.rs
@@ -16,46 +16,11 @@
     clippy::result_unwrap_used
 )]
 
+mod driver;
 mod valgrind_xml;
 
-use std::env;
-use std::path::Path;
-use std::process::Command;
-
 fn main() {
-    /* get path of `rustc` */
-    let cargo = env::var_os("CARGO").expect("CARGO environment variable is not set");
-    let rustc = Path::new(&cargo).with_file_name("rustc");
-
-    /* get the output of `rustc -vV` */
-    let rustc_info = Command::new(rustc).arg("-vV").output().unwrap().stdout;
-
-    /* get the host information (all after the "host: ..." line) */
-    let host = rustc_info
-        .windows(5)
-        .position(|window| window == b"host:")
-        .expect("Host information not present in `rustc -vV`");
-    let host: String = rustc_info
-        .into_iter()
-        .skip(host)
-        .skip(b"host: ".len())
-        .take_while(|&x| x != b'\n')
-        .map(char::from)
-        .collect();
-
-    /* convert to runner env variable */
-    let runner = format!(
-        "CARGO_TARGET_{}_RUNNER",
-        host.replace('-', "_").replace('.', "_").to_uppercase()
-    );
-
-    /* cargo run with a custom runner */
-    Command::new(cargo)
-        .args(env::args_os().skip(2))
-        .envs(env::vars_os())
-        .env(runner, "valgrind") // TODO: call the own binary with a special parameter
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
+    if !driver::driver().expect("Could not execute subcommand") {
+        std::process::exit(1);
+    }
 }
