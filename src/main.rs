8f2dcef35cd927afd6f1627ce9f0d3b8d2dbc0e9
Julian Frimmel <julian.frimmel@gmail.com>
2021-03-11 19:53:04 +0100

    Format the valgrind output like the old version
    This change restores the output of the old program version, before the
    custom-runner approach.

--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,11 @@
 mod driver;
 mod valgrind_xml;
 
+use colored::Colorize as _;
 use std::env;
 use std::ffi::OsString;
-use std::io::Read;
 use std::net::{SocketAddr, TcpListener};
-use std::process;
+use std::process::{self, Command};
 
 fn main() {
     if env::args_os().nth(1) == Some(OsString::from("valgrind")) {
@@ -31,26 +31,55 @@ fn main() {
             process::exit(200);
         }
     } else {
+        // we are running as the cargo runner, therefore everything except the
+        // first argument is the command to execute.
+        let command = env::args_os().skip(1);
+
         // port selected by OS
         let address: SocketAddr = ([127, 0, 0, 1], 0).into();
         let listener = TcpListener::bind(address).unwrap();
         let address = listener.local_addr().unwrap();
 
-        let mut cargo = std::process::Command::new("valgrind")
+        let mut cargo = Command::new("valgrind")
             .arg("--xml=yes")
             .arg(format!("--xml-socket={}:{}", address.ip(), address.port()))
-            .args(env::args_os().skip(1))
+            .args(command)
             .spawn()
-            .unwrap();
+            .expect("Valgrind is not installed or cannot be started");
 
-        let (mut listener, _socket) = listener.accept().unwrap();
-
+        // collect the output of valgrind
+        let (listener, _socket) = listener.accept().unwrap();
+        let xml: valgrind_xml::Output =
+            serde_xml_rs::from_reader(listener).expect("Cannot parse valgrind output");
         let success = cargo.wait().unwrap().success();
-        let mut xml = String::new();
-        listener.read_to_string(&mut xml).unwrap();
-        println!("{}", xml);
         if !success {
             process::exit(100);
+        } else if let Some(errors) = xml.errors {
+            // format the output in a helpful manner
+            for error in &errors {
+                eprintln!(
+                    "{:>12} leaked {} in {} block{}",
+                    "Error".red().bold(),
+                    bytesize::to_string(error.resources.bytes as _, true),
+                    error.resources.blocks,
+                    if error.resources.blocks == 1 { "" } else { "s" }
+                );
+                let mut info = Some("Info".cyan().bold());
+                error.stack_trace.frames.iter().for_each(|frame| {
+                    eprintln!("{:>12} at {}", info.take().unwrap_or_default(), frame)
+                });
+            }
+
+            let total: usize = errors.iter().map(|error| error.resources.bytes).sum();
+            eprintln!(
+                "{:>12} Leaked {} total",
+                "Summary".red().bold(),
+                bytesize::to_string(total as _, true)
+            );
+
+            process::exit(127);
         }
+
+        // TODO: use drop guard, that waits on child in order to prevent printing to stdout of the child
     }
 }
