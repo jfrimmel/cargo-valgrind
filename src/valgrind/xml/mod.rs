f360174dbb87c8089e3f7e09203aae4f746c30fc
TheWastl <36932506+TheWastl@users.noreply.github.com>
2022-05-29 16:34:26 +0200

    Use strong-xml instead of serde and generalize frontend
    - use strong-xml instead of serde
    - display all errors, not only leaks
    - display <auxwhat>s and additional stacks
    - use the description provided by valgrind instead of providing our own

--- src/valgrind/xml/mod.rs
+++ src/valgrind/xml/mod.rs
@@ -11,19 +11,20 @@
 #[cfg(test)]
 mod tests;
 
-use serde::{de::Visitor, Deserialize, Deserializer};
 use std::fmt::{self, Display, Formatter};
+use strong_xml::{XmlError, XmlRead, XmlReader, XmlResult};
+use strum::EnumString;
 
 /// The output of a valgrind run.
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
-#[serde(rename = "valgrindoutput")]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, XmlRead)]
+#[xml(tag = "valgrindoutput")]
 pub struct Output {
-    #[serde(rename = "protocolversion")]
+    #[xml(flatten_text = "protocolversion")]
     protocol_version: ProtocolVersion,
-    #[serde(rename = "protocoltool")]
+    #[xml(flatten_text = "protocoltool")]
     tool: Tool,
-    #[serde(rename = "error")]
-    pub errors: Option<Vec<Error>>,
+    #[xml(child = "error")]
+    pub errors: Vec<Error>,
 }
 
 /// The version of the XML format.
@@ -31,9 +32,9 @@ pub struct Output {
 /// Although there are also versions 1-3, there is only a variant for version 4,
 /// so that all older formats will fail. The other `struct`s in this file assume
 /// the newest protocol version.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, EnumString)]
 enum ProtocolVersion {
-    #[serde(rename = "4")]
+    #[strum(serialize = "4")]
     Version4,
     // other formats are not supported
 }
@@ -43,34 +44,119 @@ enum ProtocolVersion {
 /// Although there are other tools available, there is only a variant for the
 /// so-called `memcheck` tool, so that all other tools will fail. The other
 /// `struct`s in this file assume the memcheck output.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, EnumString)]
 enum Tool {
-    #[serde(rename = "memcheck")]
+    #[strum(serialize = "memcheck")]
     MemCheck,
     // other tools are not supported
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Error {
-    #[serde(deserialize_with = "deserialize_hex")]
     unique: u64,
     pub kind: Kind,
-    #[serde(default)]
-    #[serde(rename = "xwhat")]
+    pub description: String,
     pub resources: Resources,
-    #[serde(rename = "stack")]
     pub stack_trace: Stack,
+    pub extra: Vec<ErrorExtra>,
+}
+impl<'a> XmlRead<'a> for Error {
+    fn from_reader(reader: &mut XmlReader<'a>) -> XmlResult<Self> {
+        #[derive(XmlRead)]
+        enum What {
+            #[xml(tag = "what")]
+            What(#[xml(text)] String),
+            #[xml(tag = "xwhat")]
+            XWhat {
+                #[xml(flatten_text = "text")]
+                text: String,
+                #[xml(flatten_text = "leakedbytes", default)]
+                bytes: usize,
+                #[xml(flatten_text = "leakedblocks", default)]
+                blocks: usize,
+            },
+        }
+
+        #[derive(XmlRead)]
+        #[xml(tag = "error")]
+        struct TmpError {
+            #[xml(flatten_text = "unique")]
+            unique: Hex64,
+            #[xml(flatten_text = "kind")]
+            pub kind: Kind,
+            #[xml(child = "what", child = "xwhat")]
+            pub what: What,
+            #[xml(child = "stack", child = "auxwhat", child = "xauxwhat")]
+            pub extra: Vec<ErrorExtra>,
+        }
+
+        let TmpError {
+            unique: Hex64(unique),
+            kind,
+            what,
+            mut extra,
+        } = TmpError::from_reader(reader)?;
+        let first = (!extra.is_empty()).then(|| extra.remove(0));
+        let stack_trace = if let Some(ErrorExtra::StackTrace(s)) = first {
+            s
+        } else {
+            return Err(XmlError::MissingField {
+                name: "Error".to_string(),
+                field: "stack_trace".to_string(),
+            });
+        };
+        let (description, resources) = match what {
+            What::What(s) => (s, Resources::default()),
+            What::XWhat {
+                text,
+                bytes,
+                blocks,
+            } => (text, Resources { bytes, blocks }),
+        };
+        Ok(Self {
+            unique,
+            kind,
+            description,
+            resources,
+            stack_trace,
+            extra,
+        })
+    }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum ErrorExtra {
+    StackTrace(Stack),
+    AuxWhat(String),
+}
+impl<'a> XmlRead<'a> for ErrorExtra {
+    fn from_reader(reader: &mut XmlReader<'a>) -> XmlResult<Self> {
+        #[derive(XmlRead)]
+        pub enum TmpErrorExtra {
+            #[xml(tag = "stack")]
+            StackTrace(Stack),
+            #[xml(tag = "auxwhat")]
+            AuxWhat(#[xml(text)] String),
+            #[xml(tag = "xauxwhat")]
+            XAuxWhat(#[xml(flatten_text = "text")] String),
+        }
+
+        Ok(match TmpErrorExtra::from_reader(reader)? {
+            TmpErrorExtra::StackTrace(s) => Self::StackTrace(s),
+            TmpErrorExtra::AuxWhat(s) | TmpErrorExtra::XAuxWhat(s) => Self::AuxWhat(s),
+        })
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, EnumString)]
 pub enum Kind {
-    #[serde(rename = "Leak_DefinitelyLost")]
+    #[strum(serialize = "Leak_DefinitelyLost")]
     LeakDefinitelyLost,
-    #[serde(rename = "Leak_StillReachable")]
+    #[strum(serialize = "Leak_StillReachable")]
     LeakStillReachable,
-    #[serde(rename = "Leak_IndirectlyLost")]
+    #[strum(serialize = "Leak_IndirectlyLost")]
     LeakIndirectlyLost,
-    #[serde(rename = "Leak_PossiblyLost")]
+    #[strum(serialize = "Leak_PossiblyLost")]
     LeakPossiblyLost,
     InvalidFree,
     MismatchedFree,
@@ -106,30 +192,24 @@ impl Display for Kind {
     }
 }
 
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Resources {
-    #[serde(rename = "leakedbytes")]
     pub bytes: usize,
-    #[serde(rename = "leakedblocks")]
     pub blocks: usize,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, XmlRead)]
+#[xml(tag = "stack")]
 pub struct Stack {
-    #[serde(rename = "frame")]
+    #[xml(child = "frame")]
     pub frames: Vec<Frame>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Frame {
-    #[serde(rename = "ip")]
-    #[serde(deserialize_with = "deserialize_hex")]
     pub instruction_pointer: u64,
-    #[serde(rename = "obj")]
     pub object: Option<String>,
-    #[serde(rename = "dir")]
     pub directory: Option<String>,
-    #[serde(rename = "fn")]
     pub function: Option<String>,
     pub file: Option<String>,
     pub line: Option<usize>,
@@ -148,26 +228,88 @@ impl Display for Frame {
         Ok(())
     }
 }
+impl<'a> XmlRead<'a> for Frame {
+    fn from_reader(reader: &mut XmlReader<'a>) -> XmlResult<Self> {
+        #[derive(XmlRead)]
+        #[xml(tag = "frame")]
+        pub struct TmpFrame {
+            #[xml(flatten_text = "ip")]
+            pub instruction_pointer: Hex64,
+            #[xml(flatten_text = "obj")]
+            pub object: Option<String>,
+            #[xml(flatten_text = "dir")]
+            pub directory: Option<String>,
+            #[xml(flatten_text = "fn")]
+            pub function: Option<String>,
+            #[xml(flatten_text = "file")]
+            pub file: Option<String>,
+            #[xml(flatten_text = "line")]
+            pub line: Option<usize>,
+        }
 
-fn deserialize_hex<'de, D: Deserializer<'de>>(deserializer: D) -> Result<u64, D::Error> {
-    deserializer.deserialize_str(HexVisitor)
+        let TmpFrame {
+            instruction_pointer: Hex64(instruction_pointer),
+            object,
+            directory,
+            function,
+            file,
+            line,
+        } = TmpFrame::from_reader(reader)?;
+        Ok(Self {
+            instruction_pointer,
+            object,
+            directory,
+            function,
+            file,
+            line,
+        })
+    }
 }
 
-/// A visitor for parsing a `u64` in the format `0xDEADBEEF`.
-struct HexVisitor;
-impl<'de> Visitor<'de> for HexVisitor {
-    type Value = u64;
+use hex64::Hex64;
+mod hex64 {
+    use std::error::Error;
+    use std::fmt::{self, Display, Formatter};
+    use std::num::ParseIntError;
+    use std::str::FromStr;
 
-    fn expecting(&self, f: &mut Formatter) -> fmt::Result {
-        f.write_str("hexadecimal number with leading '0x'")
+    #[derive(Debug)]
+    pub enum ParseHex64Error {
+        MissingPrefix,
+        ParseInt(ParseIntError),
     }
 
-    fn visit_str<E: serde::de::Error>(self, value: &str) -> Result<Self::Value, E> {
-        let value = value.to_ascii_lowercase();
-        let value = value
-            .strip_prefix("0x")
-            .ok_or_else(|| E::custom("'0x' prefix missing"))?;
-        Self::Value::from_str_radix(value, 16)
-            .map_err(|_| E::custom(format!("invalid hex number '{}'", value)))
+    impl Display for ParseHex64Error {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            match self {
+                Self::MissingPrefix => write!(f, "HEX64: missing 0x prefix"),
+                Self::ParseInt(err) => write!(f, "HEX64: {}", err),
+            }
+        }
+    }
+
+    impl Error for ParseHex64Error {
+        fn source(&self) -> Option<&(dyn Error + 'static)> {
+            match self {
+                Self::MissingPrefix => None,
+                Self::ParseInt(err) => Some(err),
+            }
+        }
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    pub struct Hex64(pub u64);
+    impl FromStr for Hex64 {
+        type Err = ParseHex64Error;
+
+        fn from_str(value: &str) -> Result<Self, Self::Err> {
+            let value = value.to_ascii_lowercase();
+            let value = value
+                .strip_prefix("0x")
+                .ok_or(ParseHex64Error::MissingPrefix)?;
+            u64::from_str_radix(value, 16)
+                .map(Hex64)
+                .map_err(ParseHex64Error::ParseInt)
+        }
     }
 }
